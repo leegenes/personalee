af58e86d1e625875c651347fac5453b6097fe92e
Lee Haugen <haugenlee@leegenes.local>
2017-07-24 15:12:43 -0400

    add project tools and github link to projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,17 +1,50 @@
 import React, { Component } from 'react';
 
+const Tool = (props) => {
+  return (
+    <text className="project-tool small">
+      { props.toolName }
+    </text>
+  )
+}
+
+const ToolList = (props) => {
+  if (!props.tools) {
+    return (
+      <div></div>
+    )
+  }
+  const toolItems = Object.keys(props.tools).map((key) => {
+    return (
+      <Tool toolName={ props.tools[key] } />
+    )
+  })
+
+  return (
+    <div className="project-tools container">
+      { toolItems }
+    </div>
+  )
+}
 
 
 const Project = (props) => {
   return (
     <div className="project container vertical">
-      <div className="project-info title header">{ props.project.title}</div>
+      <text className="project-info title header">{ props.project.title}</text>
       <a className="image-link" href={props.externalURL}>
         <img className="project-info image" src={ props.project.photoURL }></img>
       </a>
-      <text className="project-info desc">{ props.project.description }</text>
-      <div className="project-info tools">
-
+      <div className="project-info container vertical">
+        <text className="project-desc">{ props.project.description }</text>
+        <div className="project-meta container">
+          <text className="git-link bordered">
+            <a href={ props.project.gitURL }>
+              <i className="fa fa-github" aria-hidden="true"></i> See it on Github
+            </a>
+          </text>
+          <ToolList tools={ props.project.tools } />
+        </div>
       </div>
     </div>
   )
